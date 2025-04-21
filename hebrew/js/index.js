9f584432bb9ecb76c45189a548e3dd78f0ff84a2
Felix Broman <felix.broman@web.de>
2025-04-22 01:18:26 +0200

    Multiple scripts

--- hebrew/js/index.js
+++ hebrew/js/index.js
@@ -1,47 +1,69 @@
-const NAMES = ['alef', 'bet', 'gimel', 'dalet', 'he', 'vav', 'zayin', 'chet', 'tet', 'yod', 'kaf', 'lamed', 'mem', 'nun', 'samekh', 'ayin', 'pe', 'tsadi', 'qof', 'resh', 'shin', 'tav'];
-const SOFIT_NAMES = ['kaf', 'mem', 'nun', 'pe', 'tsadi'];
-const ALEF_CHARCODE = 0x5d0;
+// --------------- CONSTANTS -----------------
+const SCRIPTS = {};
+const TEMPLATES = {
+    button: `
+        <input type="{TYPE}" class="btn-check form-input {CLASS}" name="{NAME}" id="{ID}" autocomplete="off" value="{VALUE}" {CHECKED}>
+        <label class="btn btn-outline-primary" for="{ID}">{DISPLAYNAME}</label>
+    `,
+};
+const scriptSelect = document.getElementById("scriptSelect");
 
-const LETTERS = {};
-let i = 0;
-for (const name of NAMES) {
-    obj = {};
-    obj.name = name;
-    if (SOFIT_NAMES.indexOf(name) != -1) {
-        obj.sofitchar = String.fromCharCode(ALEF_CHARCODE + i);
-        i += 1
-    }
-    obj.char = String.fromCharCode(ALEF_CHARCODE + i);
-    i += 1;
-    LETTERS[name] = obj;
-}
-// console.log(JSON.stringify(LETTERS));
 
-const nameInput = document.getElementById("nameInput");
-
+// --------------- GAME CLASS -----------------
 class Game {
     constructor() {
-        this.reset_chars();
+        this.reset();
+    }
+
+    reset() {
+        this.script = scriptSelect.value;
+        this.chars = [];
         this.reset_score();
+        
+        for (const id of ["cur", "entered", "sol"]) {
+            this.display_char(id, "");
+        }
+        document.querySelectorAll(".eval-field").forEach(elem => {
+            elem.innerHTML = "";
+            elem.classList.remove("correct", "incorrect");
+        });
+    }
+
+    start() {
+        this.setup_chars();
+        this.setup_round();
+    }
+
+    restart() {
+        this.reset();
+        this.start();
+    }
+
+    fonts() {
+        return SCRIPTS[this.script].fonts;
+    }
+
+    forms() {
+        return SCRIPTS[this.script].forms;
     }
 
     display_char(id, char = null) {
         if (char === null) {
             char = this.sol.char;
         }
-        document.getElementById("display-" + id).getElementsByClassName('char')[0].innerText = char;
+        document.getElementById("display-" + id).getElementsByClassName('char')[0].innerHTML = char;
     }
 
     setup_round() {
         let index = randindex(this.chars);
         this.sol_index = index;
         this.sol = this.chars[index];
+
+        this.display_score();
         
         this.display_char("cur", this.sol.char);
         nameInput.value = "";
         nameInput.focus();
-
-        console.log(this.chars.length);
     }
 
     eval_round() {
@@ -49,7 +71,7 @@ class Game {
         let entered = nameInput.value;
         this.display_entered_char(entered);
         const [evalEntered, evalSol] = get_eval_elements(nameInput);
-        console.log(nameInput.nextSibling);
+        
         evalEntered.innerText = entered;
         evalSol.innerText = this.sol.name;
 
@@ -61,15 +83,17 @@ class Game {
 
     display_entered_char(value) {
         value = value.toLowerCase();
-        if (value in LETTERS) {
-            let letter = LETTERS[value];
-            let char = letter.char
-            if (letter.sofitchar) {
-                char += letter.sofitchar;
+        if (value in this.letters) {
+            let letter = this.letters[value];
+            let joined_chars = "";
+            for (const char of Object.values(letter.chars)) {
+                joined_chars += char;
             }
-            this.display_char("entered", char);
+            joined_chars = joined_chars.match(/.{1,2}/g).join("<br>");
+            this.display_char("entered", joined_chars);
+
             classIfElse(
-                letter.sofitchar,
+                joined_chars.length == 2,
                 document.getElementById("display-entered").querySelector(".char"),
                 "two-chars"
             );
@@ -80,16 +104,16 @@ class Game {
     }
 
     clear_char(correct) {
-        let removeCleared = this.setting("removeCleared")
+        let removeCleared = this.setting("removeCleared");
         if (correct && removeCleared) {
             this.remove_char(this.sol_index);
         }
         if (this.chars.length == 0 || !removeCleared && !this.chars_complete) {
-            this.reset_chars();
+            this.setup_chars();
         }
     }
 
-    next_round() {
+    submit() {
         this.eval_round();
         this.setup_round();
     }
@@ -97,13 +121,11 @@ class Game {
     reset_score() {
         this.points = 0;
         this.total = 0;
-        this.display_score();
     }
 
     update_score(points = 0) {
         this.points += points;
         this.total += 1;
-        this.display_score();
     }
 
     display_score() {
@@ -113,16 +135,34 @@ class Game {
     }
 
     generate_chars() {
-        let chars = NAMES.map(name => {return {'name': name, 'char': LETTERS[name].char}})
-        if (this.setting("sofit")) {
-            chars = chars.concat(
-                SOFIT_NAMES.map(name => {return {'name': name, 'char': LETTERS[name].sofitchar}})
-            );
+        let chars = [];
+        for (const letter of Object.values(this.letters)) {
+            for (const [form, char] of Object.entries(letter.chars)) {
+                if (this.active_forms.includes(form)) {
+                    chars.push({name: letter.name, char: char});
+                }
+            }
         }
+        
         return chars;
     }
 
-    reset_chars() {
+    update_active_forms() {
+        this.active_forms = [];
+        document.querySelectorAll(".form-button").forEach(elem => {
+            if (elem.checked) {
+                this.active_forms.push(elem.value);
+            }
+        });
+    }
+
+    setup_chars() {
+        this.letters = {};
+        for (const letter of SCRIPTS[this.script].letters) {
+            this.letters[letter.name] = letter;
+        }
+
+        this.update_active_forms();
         this.chars = this.generate_chars();
         this.chars_complete = true;
     }
@@ -137,6 +177,40 @@ class Game {
     }
 }
 
+
+// --------------- GAME SETUP -----------------
+const game = new Game();
+
+fetch("./json/scripts.json")
+    .then(response => response.json())
+    .then(json => {
+        Object.assign(SCRIPTS, json);
+        setup_game();
+    });
+
+const nameInput = document.getElementById("nameInput");
+const fontButtons = document.getElementById("fontButtons");
+const formsButtons = document.getElementById("formsButtons");
+
+scriptSelect.addEventListener("change", setup_game);
+
+function setup_game() {
+    let scripttype = SCRIPTS[scriptSelect.value].type;
+    let gamename = scripttype + " Game";
+    document.querySelectorAll("h2, title").forEach(elem => {
+        elem.innerText = gamename;
+    });
+
+    game.reset();
+    setup_font_buttons();
+    setup_forms_buttons();
+    update_font();
+    game.start();
+}
+
+
+
+// --------------- EVENT LISTENERS -----------------
 document.querySelectorAll("input[type=range]").forEach((elem) => {
     let span = document.querySelector(`label[for="${elem.id}"] .range-value`);
     if (span) {
@@ -150,8 +224,6 @@ document.getElementById("fontWeightRange").addEventListener("input", (e) => {
     set_global_css_var('char-font-weight', e.target.value);
 });
 
-const game = new Game();
-
 document.querySelectorAll("input[type=text]").forEach((input) => {
     input.addEventListener("focus", (e) => {
         e.preventDefault();
@@ -161,16 +233,10 @@ document.querySelectorAll("input[type=text]").forEach((input) => {
 
 nameInput.addEventListener("keydown", (e) => {
     if (e.key == "Enter") {
-        game.next_round();
+        game.submit();
     }
 });
 
-game.setup_round();
-
-const fonts = {
-    'serif': {family: '"Noto Serif Hebrew", serif', shift: '0'},
-    'sans': {family: '"Noto Sans Hebrew", sans-serif', shift: '0.18em'}
-};
 document.querySelectorAll(".font-input").forEach((elem) => {
     let font = fonts[elem.value];
     elem.addEventListener("click", () => {
@@ -179,10 +245,71 @@ document.querySelectorAll(".font-input").forEach((elem) => {
     });
 });
 
+formsButtons.addEventListener("click", (e) => {
+    if (e.target.classList.contains("form-button")) {
+        game.restart();
+    }
+});
+
+function update_font(index = 0) {
+    let font = game.fonts()[index];
+    set_global_css_var("--char-font-family", font.family);
+    
+    let shift = game.script == "hebrew" && font.displayname == "Sans" ? "0.18em" : 0;
+    set_global_css_var("--char-shift", shift);
+    
+    let scale = game.script == "greek" ? 0.85 : 1;
+    set_global_css_var("--char-scale", scale);
+}
+
+function setup_font_buttons() {
+    let html = "";
+    for (const [index, font] of Object.entries(game.fonts())) {
+        html += html_from_template("button", {
+            type: "radio",
+            name: "font-button",
+            checked: index == 0 ? "checked" : "",
+            class: "font-button",
+            id: "fontButton" + index,
+            value: index,
+            displayname: font.displayname
+        });
+    }
+    fontButtons.innerHTML = html;
+    fontButtons.querySelectorAll(".font-button").forEach(elem => {
+        elem.addEventListener("change", () => {
+            if (elem.checked) {
+                update_font(elem.value);
+            }
+        })
+    })
+}
+
+function setup_forms_buttons() {
+    let html = "";
+    for (const [index, form] of Object.entries(game.forms())) {
+        html += html_from_template("button", {
+            type: "checkbox",
+            name: "form-button",
+            checked: "checked",
+            class: "form-button",
+            id: "formButton" + index,
+            value: form.key,
+            displayname: form.displayname
+        });
+    }
+    formsButtons.innerHTML = html;
+}
 
 
-
-
+// --------------- DOM FUNCTIONS ---------------
+function html_from_template(template_name, data) {
+    let html = TEMPLATES[template_name];
+    for (const [key, value] of Object.entries(data)) {
+        html = html.replaceAll(`{${key.toUpperCase()}}`, value);
+    }
+    return html;
+}
 
 function get_eval_elements(input) {
     if (typeof input === "string") {
@@ -190,7 +317,6 @@ function get_eval_elements(input) {
     }
     return [input.nextElementSibling, input.nextElementSibling.nextElementSibling];
 }
-
 
 function set_global_css_var(property, value) {
     if (property.substr(0, 2) != "--") {
@@ -223,6 +349,7 @@ function split_classes(classstr) {
     return classstr.split(" ");
 }
 
+// --------------- RANDOM FUNCTIONS -----------------
 function randint(min, max = null) {
     if (max === null) {
         max = min;
